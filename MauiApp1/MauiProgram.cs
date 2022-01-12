c2b387bf76f4277b9d0aac6226b4b16f0ae0bc6a
mALIk sHAHId <mALIk_BsHAHId@outlook.com>
2022-01-12 13:55:57 +0530

    Add project files.

---
+++ MauiApp1/MauiProgram.cs
@@ -0,0 +1,30 @@
+﻿using BlazorClient.Data;
+using Microsoft.AspNetCore.Components.WebView.Maui;
+using Microsoft.EntityFrameworkCore;
+
+namespace MauiApp1
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .RegisterBlazorMauiWebView()
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddBlazorWebView();
+            builder.Services.AddDbContext<ProductDbContext>(options =>
+            {
+                options.UseSqlite("Data Source = Products.db");
+            });
+            builder.Services.AddScoped<ProductServices>();
+
+            return builder.Build();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# blazorservercrudefsqlite
+Example project to implement CRUD operation in SQLite database using EntityFrameworkCore
+
+![](BlazorCrudSqlite.gif)
+
+The complete details can be found at the blog post: https://executecommands.com/crud-in-blazor-using-sqlite-entity-framework/
